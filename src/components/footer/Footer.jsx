2cfad81584c8bb49c8a86063196642d206acbb21
SFERAS <rthyssen73@gmail.com>
2024-01-12 23:56:01 +0100

    footer on progress

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -21,25 +21,16 @@ function Footer() {
 	return (
 		<div className='footer'>
 			<div className="footer__operation footer__operation--block">
-				<h2>HOURS OF OPERATION </h2>
+				<h3>HOURS OF OPERATION </h3>
 
 				<p>Mon-Thu: 9AM to 8PM</p>
 				<p>Friday: 9AM to 3PM</p>
 				<p>Sat-Sun: Closed</p>
 
-				{/* <div className="footer__operation"> */}
-				{/* <h2>{trans[lang].footerServices}</h2>
-						<NavLink to="SoftwareDevelopment" onClick={scrollToTheTop}>{trans[lang].footerSoftware}</NavLink>
-						<NavLink to="E_Commerce" onClick={scrollToTheTop}>{trans[lang].e_commerceDevelopment}</NavLink>
-						<NavLink to="CloudHosting" onClick={scrollToTheTop}>{trans[lang].footerCloud} </NavLink>
-						<NavLink to="WebDevelopment" onClick={scrollToTheTop}>{trans[lang].footerWeb}</NavLink>
-						<NavLink to="MobileDevelopment" onClick={scrollToTheTop}>{trans[lang].footerMobile}</NavLink>
-						<NavLink to="UX_UI" onClick={scrollToTheTop}>UX/UI</NavLink> */}
-				{/* </div> */}
 			</div>
 
 			<div className="footer__contact footer__operation--block">
-				<h2>CONTACT ME</h2>
+				<h3>CONTACT ME</h3>
 
 				<p>500 Terry Francine Street</p>
 				<p>San Francisco, CA 94158</p>
@@ -47,22 +38,10 @@ function Footer() {
 				<p>Mail: info@mysite.com</p>
 				<p>Tel: 123-456-7890</p>
 
-				{/* <NavLink to="Angular" onClick={scrollToTheTop}>Angular</NavLink>
-				<NavLink to="Vue" onClick={scrollToTheTop}>Vue.js</NavLink>
-				<NavLink to="React_Native" onClick={scrollToTheTop}>React Native</NavLink>
-				<NavLink to="JS" onClick={scrollToTheTop}>JavaScript</NavLink>
-				<NavLink to="AWS" onClick={scrollToTheTop}>AWS</NavLink>
-				<NavLink to="Microsoft_Azure" onClick={scrollToTheTop}>Microsoft Azure</NavLink>
-				<NavLink to="Figma" onClick={scrollToTheTop}>Figma</NavLink>
-				<NavLink to="Python" onClick={scrollToTheTop}>Python</NavLink>
-				<NavLink to="MUI" onClick={scrollToTheTop}>Material-UI</NavLink>
-				<NavLink to="Swift" onClick={scrollToTheTop}>Swift</NavLink>
-				<NavLink to="MySQL" onClick={scrollToTheTop}>MySQL</NavLink>
-				<NavLink to="CPlusPlus" onClick={scrollToTheTop}>C++</NavLink> */}
 			</div>
 			
 			<div className="footer__menu footer__operation--block">
-				<h2>MENU</h2>
+				<h3>MENU</h3>
 
 				<p><IoDiamondSharp /> About</p>
 				<p><IoDiamondSharp /> Services</p>
@@ -71,12 +50,6 @@ function Footer() {
 				<p><IoDiamondSharp /> Contact</p>
 
 				<NavLink className="footer__menu--link-to-contact" to="Contact" onClick={scrollToTheTop}>{trans[lang].footerContact}</NavLink>
-
-				{/* <NavLink to="services" onClick={scrollToTheTop}>{trans[lang].services}</NavLink>
-				<NavLink to="How" onClick={scrollToTheTop}>{trans[lang].how}</NavLink>
-				<NavLink to="Careers" onClick={scrollToTheTop}>{trans[lang].footerCarrers}</NavLink>
-				<NavLink to='About' onClick={scrollToTheTop}>{trans[lang].footerAbout}</NavLink>
-				<NavLink to="Contact" onClick={scrollToTheTop}>{trans[lang].footerContact}</NavLink> */}
 			</div>
 
 			<div className="footer__logo footer__operation--block">
@@ -90,24 +63,6 @@ function Footer() {
 				</div>
 
 				<p>© 2024 by TISSEN  Powered</p>
-				{/* <p>and secured by Wix</p> */}
-
-				{/* <div className="footer__container--section-2-logos">
-					<NavLink to="*" onClick={scrollToTheTop}> PIPI sports Psychology </NavLink>
-					<a href='https://www.instagram.com/tissentechnologies/?igshid=MzRlODBiNWFlZA%3D%3D/' target='_blank'><FaInstagram /></a>
-					<a href='https://twitter.com/tissentech' target='_blank'><FaTwitter /></a>
-					<a href='https://www.facebook.com/profile.php?id=61550480973075' target='_blank'><FaFacebookF /></a>
-					<a href='https://www.linkedin.com/company/tissen/about/?viewAsMember=true' target='_blank'><BsLinkedin /></a>
-				</div>
-				<div className="footer__container--section-2-email">
-					<h3>{trans[lang].footerEmail}</h3>
-					<a href="mailto:info@tissen.tech">info@tissen.tech</a>
-				</div>
-				<div className="footer__container--section-2-phone">
-					<h3>{trans[lang].footerPhone}</h3>
-					<p>AR (+54) 11-5763-9301 </p>
-					<p>DE (+49) 176-8315-7254 </p>
-				</div> */}
 			</div>
 		</div>
 	)
