064f29251f4019964ca3f80ff631001a3357ce45
SFERAS <rthyssen73@gmail.com>
2024-01-21 22:19:29 +0100

    section about is ready

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,16 +6,15 @@ function About() {
 		<div className="about__container">
 
 			<div className="about__container--text-container">
-				<p className="about__container--text-container__title">ABOUT PIPI</p>
-				<div className="about__container--text-container__underline-dig"></div>
-				<div className="about__container--text-container__underline-thin"></div>
-				<div className="about__container--text-container__text-about">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</div>
+				<div className="about__container--text-container--box">
+					<p className="about__container--text-container__title"><span>ABOUT</span> <span>PIPI</span></p>
+					<div className="about__container--text-container__underline-dig"></div>
+					<div className="about__container--text-container__underline-thin"></div>
+					<div className="about__container--text-container__text-about">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</div>
+				</div>
 			</div>
 
 			<div className="about__container--image-container">
-				<div className="about__container--image-container-img">
-					
-				</div>
 
 			</div>
 		</div>
