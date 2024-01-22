acd9c553039a81bc92ca89b6cbbc6e902b2527f4
SFERAS <rthyssen73@gmail.com>
2024-01-22 23:23:14 +0100

    MEET in progress

--- src/components/start/meetPipi/MeetPipi.jsx
+++ src/components/start/meetPipi/MeetPipi.jsx
@@ -7,24 +7,23 @@ function MeetPipi() {
 			<p className="meet-pipi__container--title">meet the team</p>
 			<div className="meet-pipi__container-1">
 				<div className="meet-pipi__container-1--img">
-
+					<div className="meet-pipi__container-1--img--container">					</div>
 				</div>
 				<div className="meet-pipi__container-1--text">
 					<h3>Coach Priscyla Tosti </h3>
 					<h6>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. </h6>
-				<button></button>
+					<button></button>
 				</div>
 			</div>
 
 			<div className="meet-pipi__container-2">
 				<div className="meet-pipi__container-2--img">
-
+					<div className="meet-pipi__container-2--img--container">					</div>
 				</div>
 				<div className="meet-pipi__container-2--text">
 					<h3>IT Ruslan Tissen </h3>
 					<h6>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. </h6>
 					<button></button>
-
 				</div>
 			</div>
 
