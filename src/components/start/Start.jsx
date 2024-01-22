2957ab3ecb62d42727866dd4faeae2a9d795c5a8
SFERAS <rthyssen73@gmail.com>
2024-01-22 23:13:14 +0100

    meet team started

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Start.scss"
 import Introduction from './introduction/Introduction'  
 import About from './about/About.jsx'
-// import ByServices from './byServices/ByServices.jsx';
+import MeetPipi from './meetPipi/MeetPipi.jsx'
 // import Technologies from './technologies/Technologies.jsx';
 // import Statement from './statement/Statement.jsx';
 // import Location from './locaion/Location.jsx';
@@ -14,7 +14,7 @@ function Start() {
 
 			<About />
 
-			{/* <ByServices /> */}
+			<MeetPipi />
 
 			{/* <Technologies /> */}
 
---
+++ src/components/start/meetPipi/MeetPipi.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./MeetPipi.scss"
+
+function MeetPipi() {
+	return (
+		<div className='meet-pipi__container'>
+			<p className="meet-pipi__container--title">meet the team</p>
+			<div className="meet-pipi__container-1">
+				<div className="meet-pipi__container-1--img">
+
+				</div>
+				<div className="meet-pipi__container-1--text">
+					<h3>Coach Priscyla Tosti </h3>
+					<h6>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. </h6>
+				<button></button>
+				</div>
+			</div>
+
+			<div className="meet-pipi__container-2">
+				<div className="meet-pipi__container-2--img">
+
+				</div>
+				<div className="meet-pipi__container-2--text">
+					<h3>IT Ruslan Tissen </h3>
+					<h6>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. </h6>
+					<button></button>
+
+				</div>
+			</div>
+
+		</div>
+	)
+}
+
+export default MeetPipi
