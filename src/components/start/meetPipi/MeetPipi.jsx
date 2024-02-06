040c2e5758c8b8e97ccbab9ee97fe3679fce1d68
SFERAS <rthyssen73@gmail.com>
2024-02-06 23:40:28 +0100

    button in MEET done

--- src/components/start/meetPipi/MeetPipi.jsx
+++ src/components/start/meetPipi/MeetPipi.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import "./MeetPipi.scss"
+import { NavLink } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa'
+
+
+const scrollToTheTop = () => {
+	window.scroll(0, 0)
+}
 
 function MeetPipi() {
+
 	return (
 		<div className='meet-pipi__container'>
 			<div className="meet-pipi__container-title-box">
@@ -12,11 +20,14 @@ function MeetPipi() {
 
 			<div className="meet-pipi__container-1">
 				<div className="meet-pipi__container-1--img-box">				</div>
-				
+
 				<div className="meet-pipi__container-1--text-box">
-					<p className='meet-pipi__container-1--text-box__name'>Coach Priscyla Tosti </p>
-					<p className='meet-pipi__container-1--text-box__info'>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. </p>
-					<button className='meet-pipi__container-1--text-box__btn'></button>
+					<p className='meet-pipi__container-1--text-box--name'>Coach Priscyla Tosti </p>
+					<p className='meet-pipi__container-1--text-box--info'>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. </p>
+					<div className='meet-pipi__container-1--text-box--btn'>
+						<NavLink className="meet-pipi__container-1--text-box--btn--white" to="Contact" onClick={scrollToTheTop}>Book now </NavLink>
+						<NavLink className="meet-pipi__container-1--text-box--btn--gray" to="Contact" onClick={scrollToTheTop}><FaArrowRight /> </NavLink>
+					</div>
 				</div>
 			</div>
 
@@ -25,9 +36,12 @@ function MeetPipi() {
 					<div className="meet-pipi__container-2--img--container">					</div>
 				</div>
 				<div className="meet-pipi__container-2--text-box">
-					<h3>IT Ruslan Tissen </h3>
-					<h6>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. </h6>
-					<button></button>
+					<p>IT Ruslan Tissen </p>
+					<p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. </p>
+					<div className='meet-pipi__container-2--text-box--btn'>
+						<NavLink className="introduction__container--button-text" to="Contact" onClick={scrollToTheTop}>Book now </NavLink>
+						<NavLink className="introduction__container--button-arrow" to="Contact" onClick={scrollToTheTop}><FaArrowRight /> </NavLink>
+					</div>
 				</div>
 			</div>
 
