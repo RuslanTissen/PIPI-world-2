0c432e81e1966b0f6d76d240f3a1796b31b75138
SFERAS <rthyssen73@gmail.com>
2024-02-16 18:52:15 +0100

    success mobile version nearly done

--- src/components/start/success/Success.jsx
+++ src/components/start/success/Success.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import "./Success.scss"
+import { useState } from 'react'
+
 
 function Success() {
+	// const [show, setShow] = useState(false)
+	// const [active, setActive] = useState(true)
+
+
+	// function eddText() {
+	// 	setShow(!show)
+	// 	setActive(!active)
+	// }
+
+	// const arrowDown = "↓"
+	// const arrowUp = "^"
+
 	return (
 		<div className='success'>
 			<div className="success__cont">
 				<div className="success__cont--1">
 					<h4>SUCCESS MINDSET</h4>
-					<div className="price_box">800</div>
+					<div className="price_box"><span>$</span> 800</div>
 					<h6>12 Week Intensive Program</h6>
 					<p>Valid for 12 weeks</p>
 					<button className='slct__bnt'>SELECT</button>
 				</div>
 
-				<div className="success__cont--2">
+				{/* <div className="tic" onClick={() => eddText()}>
+					{active ? <button>arrowDown</button> : <button>ArrowUp</button>}
+				</div> */}
+
+				{/* {show && */}
+				 <div className="success__cont--2">
+
 					<ul>
 						<li>Build Visualization and Goal-Setting Techniques</li>
 						<li>Narrow Your Focus</li>
@@ -23,6 +43,8 @@ function Success() {
 						<li>Learn the Art of Rehabilitation</li>
 					</ul>
 				</div>
+				{/* } */}
+
 			</div>
 		</div>
 	)
