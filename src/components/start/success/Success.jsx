2731d7fad4e037afb110252751d818d6344a7388
SFERAS <rthyssen73@gmail.com>
2024-02-16 12:57:20 +0100

    start  16 feb

--- src/components/start/success/Success.jsx
+++ src/components/start/success/Success.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import "./Success.scss"
 
 function Success() {
-  return (
-	 <div>Success</div>
-  )
+	return (
+		<div className='success'>
+			<div className="success__cont">
+				<div className="success__cont--1">
+					<h4>SUCCESS MINDSET</h4>
+					<div className="price_box">800</div>
+					<h6>12 Week Intensive Program</h6>
+					<p>Valid for 12 weeks</p>
+					<button className='slct__bnt'>SELECT</button>
+				</div>
+
+				<div className="success__cont--2">
+					<ul>
+						<li>Build Visualization and Goal-Setting Techniques</li>
+						<li>Narrow Your Focus</li>
+						<li>Gain Access to 10 Success Modules</li>
+						<li>Improve Motivation and Team-Building Skills</li>
+						<li>Conquer Anxiety</li>
+						<li>Learn the Art of Rehabilitation</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Success
